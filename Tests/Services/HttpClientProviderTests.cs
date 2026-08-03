6ad47f4d6aaac84a7419bc63e6ad2e7dda661962
Ardysa <felinscarlet@gmail.com>
2026-08-03 14:23:38 +0700

    2.2.19-beta | Bugfix | back off offline policy probes, harden remote config parsing | 2270

--- Tests/Services/HttpClientProviderTests.cs
+++ Tests/Services/HttpClientProviderTests.cs
@@ -14,7 +14,6 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
-using System.Linq;
 using ArdysaModsTools.Core.Services.Update.Models;
 using ArdysaModsTools.Helpers;
 using NUnit.Framework;
